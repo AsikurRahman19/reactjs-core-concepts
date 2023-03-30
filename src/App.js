f7c5377736876b4c8e7cbde4cb55e30b1e1a4fe0
AsikurRahman19 <asikur15-2373@diu.edu.bd>
2023-03-31 00:09:42 +0600

    2nd  commit

--- src/App.js
+++ src/App.js
@@ -6,28 +6,25 @@ const singer ={name:'Dr. Mahfuz'}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className='container'><h2>Hello ,i am Md.Asikur Rahman</h2></div>
-        <div className='music'>
-          <p>Name:{2222+ number}</p>
-          <p>Name:{singer.name}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+     
+      
     </div>
   );
 }
+function Person() {
+  return (<div className='Person'>
+     <h1>Md.Asikur Rahman</h1>
+     <p>Compuer Science Engineer</p>
+
+
+    </div>
+  
+  );
+}
+
 
 export default App;
